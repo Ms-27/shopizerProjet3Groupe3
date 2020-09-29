2e9a67ba5b24b8e4dcec10e72cafac109d57a680
Bertrand Rochette <package>
2020-09-29 22:17:10 +0200

    Modification PageTable et TestsCategorieTable

--- src/main/java/eqlp3g3/PageTable.java
+++ src/main/java/eqlp3g3/PageTable.java
@@ -1,5 +1,12 @@
 package eqlp3g3;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
 public class PageTable extends PageAbstractBandeau {
+	
+	@FindBy (xpath="//li[@class='active']/a[@href='http://localhost:8080/shop/shop/category/tables.html/ref=c:1,1']")
+	private WebElement titre_pagetable;
+	
 
 }
--- src/test/java/eqlp3g3/TestsCategorieTable.java
+++ src/test/java/eqlp3g3/TestsCategorieTable.java
@@ -1,5 +1,49 @@
 package eqlp3g3;
 
-public class TestsCategorieTable {
+import java.util.concurrent.TimeUnit;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import eqlp3g3.TestsCategorieTable;
+import eqlp3g3.ENavigateur;
+import eqlp3g3.SocleTechnique;
+
+public class TestsCategorieTable {
+	static Logger logger = LoggerFactory.getLogger(TestsCategorieTable.class);
+	WebDriver driver;
+	WebDriverWait wait;
+	Actions action;
+	
+	@Before
+	public void setUp() throws Exception {
+
+		driver = SocleTechnique.choisirNavigateur(logger, ENavigateur.f);
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		wait = new WebDriverWait(driver, 7000);
+		action = new Actions(driver);
+	}
+	
+	@After
+	public void tearDown() {
+		driver.quit();
+	}
+
+	
+	@Test
+	public void test() throws InterruptedException {
+		driver.get("http://grp3automobo.ddns.net:8090/shop/");
+		
+		//Pas1 Intanciation page accueil  
+		PageAccueil page_accueil = PageFactory.initElements(driver, PageAccueil.class);
+	}
+	
 }
